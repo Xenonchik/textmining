ae83b37fc56330f746bd28e1fdde61da097d95e7
Xenonchik <lordxenon@meta.ua>
2014-11-27 01:39:51 +0200

    first commit

---
+++ src/main/java/MyCrawler.java
@@ -0,0 +1,81 @@
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class MyCrawler extends WebCrawler {
+
+	private final static Pattern FILTERS = Pattern
+			.compile(".*(\\.(css|js|bmp|gif|jpe?g"
+					+ "|png|tiff?|mid|mp2|mp3|mp4"
+					+ "|wav|avi|mov|mpeg|ram|m4v|pdf"
+					+ "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
+	
+	Map<String, WebURL> visitedUrls = new HashMap<>();
+
+	/**
+	 * You should implement this function to specify whether the given url
+	 * should be crawled or not (based on your crawling logic).
+	 */
+	@Override
+	public boolean shouldVisit(WebURL url) {
+        String href = url.getURL().toLowerCase();
+        if(!href.startsWith("http://www.darklyrics.com/lyrics/manowar/")){
+            return false;
+        }
+		if(this.visitedUrls.containsKey(url.getURL())) {
+			return false;
+		} else {
+			this.visitedUrls.put(url.getURL(), url);
+		}
+
+		return !FILTERS.matcher(href).matches();
+	}
+
+	/**
+	 * This function is called when a page is fetched and ready to be processed
+	 * by your program.
+	 */
+	@Override
+	public void visit(Page page) {
+		String url = page.getWebURL().getURL();
+		System.out.println("URL: " + url);
+
+		if (page.getParseData() instanceof HtmlParseData) {
+			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+			String text = htmlParseData.getText();
+			String html = htmlParseData.getHtml();
+
+            Document doc = Jsoup.parse(html);
+            PrintWriter writer = null;
+            try {
+                writer = new PrintWriter("the-file-name.txt", "UTF-8");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            writer.println(html);
+            writer.close();
+            Elements div = doc.select("div.lyrics");
+            //Element head = doc.select("#head").first();
+			List<WebURL> links = htmlParseData.getOutgoingUrls();
+
+			System.out.println("Text length: " + text.length());
+			System.out.println("Html length: " + html.length());
+			System.out.println("Number of outgoing links: " + links.size());
+		}
+	}
+}
